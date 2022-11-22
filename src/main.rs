7408d84f20c2f12b239fed0551f3f1e200b3a79a
Kieffer Florent <kiefferflo@eisti.eu>
2022-11-22 14:26:51 +0100

    base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}",exec_command("echo Hello world"));
+}
+
+
+fn exec_command(cmd: &str) -> String {
+    let output = if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/C", cmd])
+            .output()
+            .expect("failed to execute process")
+    } else {
+        Command::new("sh")
+            .arg("-c")
+            .arg(cmd)
+            .output()
+            .expect("failed to execute process")
+    };
+    String::from_utf8(output.stdout).unwrap()
 }
